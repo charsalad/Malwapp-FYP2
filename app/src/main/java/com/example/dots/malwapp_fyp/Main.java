bedad85deed671b8c4fc831ed7619af4f81a0650
charsalad <limcharlotte.1796@gmail.com>
2018-06-11 12:29:10 +0800

    Updated up button and navigation button to scanning activity

--- app/src/main/java/com/example/dots/malwapp_fyp/Main.java
+++ app/src/main/java/com/example/dots/malwapp_fyp/Main.java
@@ -1,13 +1,32 @@
 package com.example.dots.malwapp_fyp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_scan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScanning();
+            }
+        });
     }
+
+    public void openScanning(){
+      Intent intent = new Intent(this, Scanning.class);
+      startActivity(intent);
+    }
+
+
 }
